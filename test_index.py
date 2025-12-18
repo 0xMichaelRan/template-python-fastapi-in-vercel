edfa53d7cbead69ec580d694b63fbed3f1fa43e3
0xMichaelRan <michaelran16@gmail.com>
2025-12-19 00:01:44 +0800

    ruff format

--- test_index.py
+++ test_index.py
@@ -7,17 +7,17 @@ from src.index import root, say_hello, hello_message
 @pytest.mark.asyncio
 async def test_root():
     result = await root()
-    assert result == {'message': 'Hello World'}
+    assert result == {"message": "Hello World"}
 
 
 @pytest.mark.asyncio
 async def test_say_hello():
     result = await say_hello("John")
-    assert result == {'message': 'Hello John'}
+    assert result == {"message": "Hello John"}
 
 
 @pytest.mark.asyncio
 async def test_hello_message():
     dto = ISayHelloDto(message="Alice")
     result = await hello_message(dto)
-    assert result == {'message': 'Hello Alice'}
+    assert result == {"message": "Hello Alice"}
